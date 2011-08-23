887860ad2bd60d258d72d4c20fdf26fb18244780
Aldo Balagulan <mrawesomer34@gmail.com>
2011-08-23 12:44:22 +0800

    Another update
    - More screens
    - More crashing

--- src/com/rdft/foreveralone/AddCourse.java
+++ src/com/rdft/foreveralone/AddCourse.java
@@ -1,6 +1,7 @@
 package com.rdft.foreveralone;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,12 +15,16 @@ public class AddCourse extends Activity
 	 * for the adding of courses. (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
+	private Intent intention;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		// Activity class for adding a new course.
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addcourse); // the layout
+		
+		intention = new Intent(this, AddSchedule.class);
 		
 		// The spinner attributes
 		Spinner termsSpinner = (Spinner) findViewById(R.id.termchooser);
@@ -29,7 +34,18 @@ public class AddCourse extends Activity
 	    termsSpinner.setAdapter(adapter);
 		
 		// Button's listener
-		final Button createCourseButton = (Button)findViewById(R.id.submitcourse);
+	    final Button createSchedButton = (Button)findViewById(R.id.addsched);
+		createSchedButton.setOnClickListener(new OnClickListener() 
+		{
+			
+			public void onClick(View v) 
+			{
+				// Pat change this button to whatever you need
+				startActivity(intention);
+			}
+		});
+	    
+	    final Button createCourseButton = (Button)findViewById(R.id.submitcourse);
 		createCourseButton.setOnClickListener(new OnClickListener() 
 		{
 			
